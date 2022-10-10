612e1e37d91d47af3f984bdde253c0e2096c2a12
I'am Carlos <103902036+CarlosTwT@users.noreply.github.com>
2022-10-10 23:26:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
 require('./config')
 const { default: simpleConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
-const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
+const { state, saveState } = await useSingleFileAuthState(`./${sessionName}.json`)
 const pino = require('pino')
 const { Boom } = require('@hapi/boom')
 const fs = require('fs')
